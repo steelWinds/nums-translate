7987192d6df38ae5b28dc59833f5b44f998a39c8
ninja <kirillsurov0@gmail.com>
2022-04-10 02:59:50 +0900

    update(packages): fix import nums-to-words-ru-lang

--- src/composables/useNumsConverter.ts
+++ src/composables/useNumsConverter.ts
@@ -1,9 +1,7 @@
-import type {IConverter} from 'nums-to-words-ru-lang/types/interfaces';
 import {ref, computed} from 'vue';
-import Converter from 'nums-to-words-ru-lang';
+import {converter} from 'nums-to-words-ru-lang';
 
 export const useNumsConverter = () => {
-  const converter: IConverter = new Converter();
   const converterModel = ref('');
   const convertedWords = computed(
       () => converter.convert(Number(converterModel.value)),
